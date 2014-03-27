5c2e1e27de42857193a0db272d2f9b41591c0a23
Maximilian Tagher <feedback.tagher@gmail.com>
2014-03-26 20:50:33 -0700

    Initial Commit

---
+++ DemoApplication/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoApplication
+//
+//  Created by Maximilian Tagher on 3/26/14.
+//  Copyright (c) 2014 Heyzap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoApplication/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoApplication
+//
+//  Created by Maximilian Tagher on 3/26/14.
+//  Copyright (c) 2014 Heyzap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
